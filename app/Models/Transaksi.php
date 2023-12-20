26219d18f6bb47c1f6e3b0716bbfb91710b801f9
Zulhisham17 <zulhisham.ramli@gmail.com>
2023-12-20 17:18:32 +0800

    Menyelesaikan transaksi admin

--- app/Models/Transaksi.php
+++ app/Models/Transaksi.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Transaksi extends Model
 {
@@ -11,4 +13,18 @@ class Transaksi extends Model
 
     protected $table = 'tranksaksis';
     protected $guarded =['id'];
+
+    public function barang(): BelongsTo
+    {
+        return $this->BelongsTo(Barang::class);
+    }
+    // public function barang(): HasMany
+    // {
+    //     return $this->hasMany(Barang::class);
+    // }
+
+    public function user(): BelongsTo
+    {
+        return $this->BelongsTo(User::class);
+    }
 }
